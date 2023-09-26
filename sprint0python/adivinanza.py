cbc93ffe33576d6016f6064bf31e72c2f54b4cd5
Miguel Rodriguez <mrodriguezm22@fpcoruna.afundacion.org>
2023-09-26 09:58:23 +0200

    ejercicio 1 adivinanza

---
+++ sprint0python/adivinanza.py
@@ -0,0 +1,12 @@
+print("Adivina, adivinanza...\n¿Qué tiene el rey en la panza?\n")
+print("a. Una cucharada de melaza\nb. De los sarracenos, una lanza\nc. Un cuenco y una taza\n")
+respuesta=input("Respuesta... ")
+if respuesta=="b":
+    print("Respuesta correcta!")
+elif respuesta=="c":
+    print("Respuesta incorrecta!")
+elif respuesta=="a":
+    print("Respuesta incorrecta!")
+else:
+    print("Respuesta no válida")
+
