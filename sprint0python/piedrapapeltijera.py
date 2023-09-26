876b99aec5fefc7a84e73a192f387363eafb3c80
Miguel Rodriguez <mrodriguezm22@fpcoruna.afundacion.org>
2023-09-26 13:17:20 +0200

    ejercicio 5 piedra-papel-tijera

---
+++ sprint0python/piedrapapeltijera.py
@@ -0,0 +1,26 @@
+from random import sample
+pos=["piedra", "papel", "tijera"]
+score=0
+for i in range(5):
+    user=input("\n¿Piedra, papel o tijera? ").lower()
+
+    ai=sample(pos, 1)[0]
+    if ai==user:
+        print("Empate!")
+    elif (user==pos[0]) & (ai==pos[2]) | (user==pos[1]) & (ai==pos[0]) | (user==pos[2]) & (ai==pos[1]):
+        print("Victoria!")
+        score+=1
+    elif (user==pos[2]) & (ai==pos[0]) | (user==pos[0]) & (ai==pos[1]) | (user==pos[1]) & (ai==pos[2]):
+        print("Derrota!")
+        score-=1
+
+    print("jugador ha elegido: ", user)
+    print("computador ha elegido: ", ai)
+
+print("\nPuntuación: ", score)
+if score==0:
+    print("Has empatado")
+elif score<0:
+    print("Has perdido")
+elif score>0:
+    print("Has ganado")
