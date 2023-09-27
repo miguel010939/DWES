4b8202f26ac0ae85a86ef8a04451bfed31841e7f
Miguel Rodriguez <mrodriguezm22@fpcoruna.afundacion.org>
2023-09-27 09:07:33 +0200

    piedra-papel-tijera no se guardo y no se subio bien: completo

--- sprint0python/piedrapapeltijera.py
+++ sprint0python/piedrapapeltijera.py
@@ -1,13 +1,18 @@
 from random import sample
 pos=["piedra", "papel", "tijera"]
 score=0
+# Sin los parentesis, estos operadores logicos me dan error por el orden de las operaciones: python trata de resolverlos antes de las comparaciones, que devolverian booleano, y los intenta aplicar a las strings
+# Luego descubri que tambien existen los operadores equivalentes "and" y "or" que no dan estos problemas... Oooops
+# Un poco raro que "&" y "and" tengan distinta prioridad operacional, pero bueno.. XD
+# Resulta que no son equivalentes, Miguel: https://www.freecodecamp.org/espanol/news/operadores-basicos-en-python-con-ejemplos/ -> Operadores Bit a Bit
 for i in range(5):
     user=input("\n¿Piedra, papel o tijera? ").lower()
 
     ai=sample(pos, 1)[0]
     if ai==user:
         print("Empate!")
-    elif user==pos[0] & (ai==pos[2]) | (user==pos[1]) & (ai==pos[0]) | (user==pos[2]) & (ai==pos[1]):
+
+    elif (user==pos[0]) & (ai==pos[2]) | (user==pos[1]) & (ai==pos[0]) | (user==pos[2]) & (ai==pos[1]):
         print("Victoria!")
         score+=1
     elif (user==pos[2]) & (ai==pos[0]) | (user==pos[0]) & (ai==pos[1]) | (user==pos[1]) & (ai==pos[2]):
