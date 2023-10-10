1c61c1c81a338561945a4377092a4ee2e11b938a
Miguel Rodriguez <mrodriguezm22@fpcoruna.afundacion.org>
2023-10-10 23:36:02 +0200

    ejercicio 7: ventanas con descripciones

--- sprint1Tkinter/catalog/cell.py
+++ sprint1Tkinter/catalog/cell.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+from tkinter import Tk
 from PIL import Image, ImageTk
+from detail_window import DetailWindow
 
 class Cell:
-    def __init__(self, title, path):
+     
+    def __init__(self, title, path, description="No description available"):
         self.title=title
         self.path=path
+        self.description=description
         original=Image.open(fp=self.path)
         rescaled=original.resize((100, 100))
         # Si escribo Image.ANTIALIAS, python me devuelve:
