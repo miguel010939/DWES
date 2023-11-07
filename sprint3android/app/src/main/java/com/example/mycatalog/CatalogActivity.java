1e99d431b0348fdd70b05c419ce3fa61da3edd98
Miguel Rodriguez <mrodriguezm22@fpcoruna.afundacion.org>
2023-11-07 09:44:16 +0100

    ejercicio 2: (error) no muestra botón

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,16 +1,31 @@
 package com.example.mycatalog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CatalogActivity extends AppCompatActivity {
+    private Button button_detail;
+    private Context context= this;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_catalog);
+
+        button_detail = findViewById(R.id.ButtonDetail);
+        button_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                context.startActivity(intent);
+            }
+        });
 
     }
 }
